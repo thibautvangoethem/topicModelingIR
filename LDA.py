1f4ed836518e370899762a1f65bf6b73c4810005
MichielTe <michiel@teblick.com>
2020-12-03 15:41:09 +0100

    improved efficiency

--- LDA.py
+++ LDA.py
@@ -4,6 +4,13 @@ from random import choices
 from scipy.sparse import csr_matrix,lil_matrix,dok_matrix
 
 
+def weightsToCumWeights(weights):
+    current_total = 0.0
+    for i in range(len(weights)):
+        current_total += weights[i]
+        weights[i] = current_total
+
+
 def LDA(alpha, beta, nb_documents, words_per_document):
     """
     Generates a corpus using the LDA model
@@ -19,11 +26,11 @@ def LDA(alpha, beta, nb_documents, words_per_document):
 
     topic_distributions = []
     for i in range(len(alpha)):  # len(alpha) is equal to the number of topics
-        topic_distributions.append(dirichlet(beta))
+        topic_distributions.append(weightsToCumWeights(dirichlet(beta)))
 
     document_topic_distributions = []
     for i in range(nb_documents):
-        document_topic_distributions.append(dirichlet(alpha))
+        document_topic_distributions.append(weightsToCumWeights(dirichlet(alpha)))
 
     corpus = dok_matrix((nb_documents, len(beta)), dtype=np.int)
     choiceList_topics = range(len(alpha))
@@ -31,11 +38,11 @@ def LDA(alpha, beta, nb_documents, words_per_document):
     for i in range(nb_documents):
         document_topic_distribution = document_topic_distributions[i]
         for j in range(words_per_document):
-            topic = choices(choiceList_topics, document_topic_distribution)[0]
-            word = choices(choiceList_words, topic_distributions[topic])[0]
+            topic = choices(choiceList_topics, cum_weights=document_topic_distribution)[0]
+            word = choices(choiceList_words, cum_weights=topic_distributions[topic])[0]
             corpus[i, word] += 1
     print("done")
     return corpus
 
 #LDA([1/3]*3, [1/100]*10, 20, 10)
-LDA([1/10]*10, [1/10000]*10000, 1000, 10)
+LDA([1/10]*10, [1/100000]*10000, 1000, 10)
