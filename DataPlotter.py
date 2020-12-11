f91b374deecd5a59944d3d67d07bf73cd05331b2
MichielTe <michiel@teblick.com>
2020-12-11 20:14:05 +0100

    added topic distribution plotter function

--- DataPlotter.py
+++ DataPlotter.py
@@ -48,6 +48,23 @@ def plot_document_word_count_distribution(documents):
     plt.title('Distribution of Document Word Counts before pre-processing', fontdict=dict(size=22))
     plt.show()
 
+def plot_topic_distribution(document_topic_mixture):
+    topic_count = len(document_topic_mixture[0])
+    topic_count_distribution = [0]*topic_count
+    for document in document_topic_mixture:
+        max_topic = document.index(max(document))
+        topic_count_distribution[max_topic] += 1
+
+    plt.figure(figsize=(16, 7), dpi=160)
+    plt.bar(x=range(topic_count), height=topic_count_distribution)
+    plt.gca().set(ylabel='Number of Documents', xlabel='Topic')
+    if topic_count > 20:
+        plt.xticks(np.linspace(0, topic_count, 11))
+    else:
+        plt.xticks(range(topic_count))
+    plt.title("Number of documents per topic for %d topics"%topic_count, fontdict=dict(size=22))
+    plt.show()
+
 
 if __name__ == "__main__":
     # documents = simpleDataReader()
@@ -55,7 +72,7 @@ if __name__ == "__main__":
     # with open('obj/pre_documents.pkl', 'wb') as file:  # save document_topic_mixture to a file
     #     pickle.dump(documents, file)
     plt.rc('font', size=18)
-    with open('obj/pre_documents.pkl', 'rb') as file:
+    with open('obj/document_topic_mixture_topics.pkl', 'rb') as file:
        documents = pickle.load(file)
 
-    plot_document_word_count_distribution(documents)
+    plot_topic_distribution(documents)
